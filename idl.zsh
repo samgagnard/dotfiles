0fad1608850c3c9d0663af8a2360e68954c02a87
Sam Gagnard <sam.gagnard@gmail.com>
2013-12-23 02:46:49 -0700

    Work in progress...still getting things together

---
+++ idl.zsh
@@ -0,0 +1,6 @@
+# IDL startup variables
+#
+alias idl='/Applications/exelis/idl/bin/idl'
+alias idlde='/Applications/exelis/idl/bin/idlde'
+
+export IDL_STARTUP='~/IDLWorkspace82/startup.pro'
---
+++ lasp.zsh
@@ -0,0 +1,13 @@
+# Connecting to work servers 
+alias work='ssh -X gagnard@opsray'
+alias workl='ssh -X ldex-ops@winl25'
+
+# IDL locations
+export IDL_DEFAULT=~/IDLWorkspace82/Default/
+
+# LADEE SVN locations 
+socsvn='/Users/gagnard/SVN/LADEE/soc/socrepo/'
+mocsvn='/Users/gagnard/SVN/LADEE/moc/flight/'
+
+# LDEX SVN documentation
+ldexdocs='/Users/gagnard/SVN/LDEX/trunk/documentation/'
