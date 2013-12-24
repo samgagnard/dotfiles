ce37f9e5b32d8424b389cd558ad9d57b031aa4a6
Sam Gagnard <sam.gagnard@gmail.com>
2013-12-24 01:35:08 -0700

    Minor adjustments

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 #--------------------------------------------------------------------------------
-# AUTHOR: gagnard
+# AUTHOR: Sam Gagnard
 #
 # DESCRIPTION: 
-#     A description of the program
+#    Links the *.link files in dotfiles directory to user home directory. Script
+#    will prompt to skip/overwrite any existing versions. 
 #
 # USAGE: 
-#     program.sh [args]
+#     linkfiles.sh [--h|help]
 #
-# ARGUMENTS: 
-#     arg1 -
-#     arg2 - 
+# OPTIONS
+#     -h|--help     Show the usage message
 #
 # NOTES: 
 #--------------------------------------------------------------------------------
@@ -67,37 +67,6 @@ error() {
     printf "%b" "[${red}ERROR${normal}] ${0}: ${1:-'Unkown Error'}\n" >&2
 }
 
-cleanup() { 
-    info "Cleaning up runtime tmp files"
-}
-
-# https://gist.github.com/davejamesmiller/1965569
-function ask() {
-    
-    while true; do
-        if [ "${2:-}" = "Y" ]; then
-            prompt="Y/n"
-            default=Y
-        elif [ "${2:-}" = "N" ]; then
-            prompt="y/N"
-            default=N
-        else
-            prompt="y/n"
-            default=
-        fi
-        # Ask the question, no-answer to default 
-        read -p "$1 [$prompt] " REPLY
-        if [ -z "$REPLY" ]; then
-            REPLY=$default
-        fi
-        # Check if the reply is valid
-        case "$REPLY" in
-            Y*|y*) return 0 ;;
-            N*|n*) return 1 ;;
-        esac
-    done
-}
-
 fail() {
     error "$1"
     case $2 in
@@ -112,7 +81,6 @@ fail() {
 }
 
 check_args() {
-
     while [ ! -z "$1" ]; do
         local arg="$1"
        
@@ -147,12 +115,11 @@ link_files() {
 check_for_dotfile() {
 
     source=$1                         # e.g. ~/.dotfiles/zshrc.link 
-
     link_file=$(basename $1)          # e.g. zshrc.link
     dotfile_name=".${link_file%.*}"   # e.g. .zshrc
     dest="$HOME/$dotfile_name"        # e.g. /home/you/.zshrc
 
-    while true
+    while true  # wait for valid input
     do
         # Check for existence of non-link files, prompt action
         if [[ -e $dest ]] && ! [[ -h $dest ]]; then
@@ -161,7 +128,7 @@ check_for_dotfile() {
 
             case $answer in
                 r|R)
-                    create_backup $dotfile_name
+                    create_backup $dest
                     link_files $source $dest
                     return
                     ;;
@@ -185,23 +152,21 @@ main() {
     # Attempt to make this work the same from all locations 
     this_dir=$(dirname $0)
     cd $this_dir
+
     DOTFILES_DIR=$(pwd)    
    
     # Verify inputs
     check_args "$@"
 
     printf "\nThis program will link *.link files in the dotfiles directory to \$HOME\n"
-    printf "Note that any existing links will be rewritten\n\n"
+    printf "Note that any existing links on dotfiles will be redefined\n\n"
     
     # Search for link file in the dotfiles directory tree 
     linkfiles=$(find $DOTFILES_DIR -maxdepth 2 -name \*.link)
     for file in $linkfiles; do
         check_for_dotfile "$file" 
     done
-
-    #echo "DOTFILE SUMMARY:"
-    #ls -la $HOME/.*
-
+    echo
 }
 
 main "$@"
