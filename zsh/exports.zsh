dbde654ffb1388782b2c79fe591efa76df8e12f8
Sam Gagnard <sam.gagnard@gmail.com>
2013-12-27 11:39:03 -0700

    Added IDL default env variable

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -4,3 +4,5 @@ export PATH="/Users/gagnard/bin:$PATH"
 # At this point I've still been using MacPorts
 export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
 
+# IDL locations
+export IDL_DEFAULT=~/IDLWorkspace82/Default/
